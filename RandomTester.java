f70e51dec95899cc761037146f3ad581f014c788
Aarón Pérez Carreño <aapecargame@gmail.com>
2022-01-18 10:10:37 +0100

    generar varios numero aleatorios

--- RandomTester.java
+++ RandomTester.java
@@ -21,4 +21,58 @@ public class RandomTester
             System.out.println(aleatorio.nextInt());
         }
     }
+    
+    public void lanzarDado(){
+        boolean completado = false;
+        while(!completado){
+            Random dado = new Random();
+            int tirada = dado.nextInt(7);
+            if(tirada > 0){
+                System.out.println(tirada);
+                completado = true;
+            }
+        }
+    }
+    
+    
+    public String getRespuesta(){
+        Random numero = new Random();
+        int aleatorio = numero.nextInt(3);
+        String respuesta = "";
+        if(aleatorio == 0){
+            respuesta = "Si";
+        }
+        else if(aleatorio == 1){
+            respuesta = "No";
+        }
+        else{
+            respuesta = "Quizás";
+        }
+        return respuesta;
+    }
+    
+    public void getNumeroEntre1Y(int limite){
+        boolean completado = false;
+        while(!completado){
+            Random aleatorio = new Random();
+            int numero = aleatorio.nextInt(limite + 1);
+            if(numero > 0){
+                System.out.println(numero);
+                completado = true;
+            }
+        }
+    }
+    
+    public int getNumeroEntre(int limite, int limiteBajo){
+        boolean completado = false;
+        int numero = 0;
+        while(!completado){
+            Random aleatorio = new Random();
+            numero = aleatorio.nextInt(limite + 1);
+            if(numero > limiteBajo){
+                completado = true;
+            }
+        }
+        return numero;
+    }
 }
